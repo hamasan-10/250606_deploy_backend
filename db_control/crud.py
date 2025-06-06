4ada957340507672f90f9576074015d8c18d66b2
hamasan-10 <beach0304.m@gmail.com>
2025-06-06 20:26:25 +0900

    initial commit

---
+++ db_control/crud.py
@@ -0,0 +1,129 @@
+# uname() error回避
+import platform
+print("platform", platform.uname())
+
+
+from sqlalchemy import create_engine, insert, delete, update, select
+import sqlalchemy
+from sqlalchemy.orm import sessionmaker
+import json
+import pandas as pd
+from db_control.connect_MySQL import engine
+from db_control.mymodels_MySQL import Customers
+
+
+def myinsert(mymodel, values):
+    # idを必須にするために追加
+    if "customer_id" not in values or not values["customer_id"]: # 空文字列もFalseと評価される
+        # (実際には、ここで例外を発生させるか、エラーを示す値を返す方が良い)
+        print(f"エラー: {mymodel.__name__} の customer_id は必須であり、空にできません。")
+        return None
+    
+    # session構築
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    query = insert(mymodel).values(values)
+    try:
+        # トランザクションを開始
+        with session.begin():
+            # データの挿入
+            result = session.execute(query)
+    except sqlalchemy.exc.IntegrityError:
+        print("一意制約違反により、挿入に失敗しました")
+        session.rollback()
+
+    # セッションを閉じる
+    session.close()
+    return "inserted"
+
+
+def myselect(mymodel, customer_id):
+    # session構築
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    query = session.query(mymodel).filter(mymodel.customer_id == customer_id)
+    try:
+        # トランザクションを開始
+        with session.begin():
+            result = query.all()
+        # 結果をオブジェクトから辞書に変換し、リストに追加
+        result_dict_list = []
+        for customer_info in result:
+            result_dict_list.append({
+                "customer_id": customer_info.customer_id,
+                "customer_name": customer_info.customer_name,
+                "age": customer_info.age,
+                "gender": customer_info.gender
+            })
+        # リストをJSONに変換
+        result_json = json.dumps(result_dict_list, ensure_ascii=False)
+    except sqlalchemy.exc.IntegrityError:
+        print("一意制約違反により、挿入に失敗しました")
+
+    # セッションを閉じる
+    session.close()
+    return result_json
+
+
+def myselectAll(mymodel):
+    # session構築
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    query = select(mymodel)
+    try:
+        # トランザクションを開始
+        with session.begin():
+            df = pd.read_sql_query(query, con=engine)
+            result_json = df.to_json(orient='records', force_ascii=False)
+
+    except sqlalchemy.exc.IntegrityError:
+        print("一意制約違反により、挿入に失敗しました")
+        result_json = None
+
+    # セッションを閉じる
+    session.close()
+    return result_json
+
+
+def myupdate(mymodel, values):
+    # session構築
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    customer_id = values.pop("customer_id")
+
+    # query = "お見事！E0002の原因はこのクエリの実装ミスです。正しく実装しましょう"
+    query = (
+        update(mymodel)
+        .where(mymodel.customer_id == customer_id) # どの顧客を更新するか指定
+        .values(**values)  # どの値を更新するか指定 (values辞書を展開)
+    )
+    try:
+        # トランザクションを開始
+        with session.begin():
+            result = session.execute(query)
+    except sqlalchemy.exc.IntegrityError:
+        print("一意制約違反により、挿入に失敗しました")
+        session.rollback()
+    # セッションを閉じる
+    session.close()
+    return "put"
+
+
+def mydelete(mymodel, customer_id):
+    # session構築
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    query = delete(mymodel).where(mymodel.customer_id == customer_id)
+    try:
+        # トランザクションを開始
+        with session.begin():
+            result = session.execute(query)
+    except sqlalchemy.exc.IntegrityError:
+        print("一意制約違反により、挿入に失敗しました")
+        session.rollback()
+
+    # セッションを閉じる
+    session.close()
+    return customer_id + " is deleted"
